4e301793b5d0dd148703f6632c7a1e5485d86fa7
Ecthon Borhis <ecthon@gmail.com>
2021-08-02 21:44:20 -0400

    Add Component Summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,8 +2,28 @@ import styles from './styles.module.scss';
 
 export function Summary() {
     return (
-        <div className={styles.Container}>
-
+        <div className={styles.containerCard}>
+            <div className={`${styles.card} ${styles.income}`}>
+                <header>
+                    <p>Entradas</p>
+                    <img src="assets/income.svg" alt="Entradas" />
+                </header>
+                <strong>10.000,00</strong>
+            </div>
+            <div className={`${styles.card} ${styles.outcome}`}>
+                <header>
+                    <p>Saídas</p>
+                    <img src="assets/outcome.svg" alt="Saídas" />
+                </header>
+                <strong>3.000,00</strong>
+            </div>
+            <div className={`${styles.card} ${styles.wallet}`}>
+                <header>
+                    <p>Carteira</p>
+                    <img src="assets/wallet.svg" alt="Carteira" />
+                </header>
+                <strong>7.000,00</strong>
+            </div>
         </div>
     )
 }
