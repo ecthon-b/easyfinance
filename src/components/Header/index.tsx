f2cb3d712ddd9700725068edd096531111b0d22a
Ecthon Borhis <ecthon@gmail.com>
2021-08-05 10:55:24 -0400

    Add Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import styles from './styles.module.scss';
 
-export function Header() {
+
+interface HeaderProps {
+onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+    
     return (
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-                <a href="">LOGO</a>
-                <nav>
-                    <a href="">Login</a>
-                </nav>
-            </div>
-        </header>
+        <>
+            <header className={styles.headerContainer}>
+                <div className={styles.headerContent}>
+                    <a href="">LOGO</a>
+                    <nav>
+                        <a href="">Login</a>
+                    </nav>
+                    <button type="button" onClick={onOpenNewTransactionModal}>Nova transação</button>
+                </div>
+            </header>
+            
+        </>
     )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,34 @@
+import Modal from 'react-modal';
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header';
 import { Summary } from '../components/Summary';
 import { Transactions } from '../components/Transactions';
 
 import '../styles/global.scss';
+import { useState } from 'react';
+
+Modal.setAppElement('#root');
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Summary />
       <Transactions />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}>
+        <h2>Cadastrar nova transação</h2>
+      </Modal>
       <Component {...pageProps} />
     </>
   )
